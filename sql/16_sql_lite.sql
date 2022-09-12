9f45b055c911349882a6840e8daa550ebf6f86da
gvaishali52 <gvaishalimlk@gmail.com>
2022-09-12 19:41:18 +0530

    Create 16_sql_lite.sql

---
+++ sql/16_sql_lite.sql
@@ -0,0 +1,9 @@
+use classicmodels;
+
+GET INDEX FROM customers FROM classicmodels;
+
+SHOW INDEX FROM customers FROM classicmodels;
+
+GET INDEX FROM classicmodels.customers;
+
+SHOW INDEX FROM classicmodels.customers;
